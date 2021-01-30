7971138f318f526dde67d553fd93b090b39e663a
Bilyana Vatev <bilyanavatev@gmail.com>
2021-01-30 12:38:39 +0000

    homework problem

---
+++ src/PO2_CompanyRoster/Employee.java
@@ -0,0 +1,41 @@
+package PO2_CompanyRoster;
+
+public class Employee {
+    private String name;
+    private Double salary;
+    private String position;
+    private String department;
+    private String email;
+    private int age;
+
+    public Employee (String name,Double salary,String position,String department){
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = "n/a";
+        this.age = -1;
+    }
+    public Employee (String name,Double salary,String position,String department,String email,int age){
+        this(name,salary,position,department);
+        this.email = email;
+        this.age = age;
+    }
+    public Employee (String name,Double salary,String position,String department,String email){
+        this(name,salary,position,department);
+        this.email = email;
+        this.age = -1;
+    }
+    public Employee (String name,Double salary,String position,String department,int age){
+        this(name,salary,position,department);
+        this.age = age;
+        this.email = "n/a";
+    }
+    public double getSalary(){
+        return this.salary;
+    }
+    @Override
+    public String toString(){
+        return String.format("%s %.2f %s %d",this.name,this.salary,this.email,this.age);
+    }
+}
