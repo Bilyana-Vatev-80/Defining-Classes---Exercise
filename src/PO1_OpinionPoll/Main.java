811f83dd18062424cfaa28737d4f8bf82fe13939
Bilyana Vatev <bilyanavatev@gmail.com>
2021-01-30 10:51:07 +0000

    homework problem

---
+++ src/PO1_OpinionPoll/Main.java
@@ -0,0 +1,31 @@
+package PO1_OpinionPoll;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List <Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            String name = input.split("\\s+")[0];
+            int age = Integer.parseInt(input.split("\\s+")[1]);
+
+            if(age > 30){
+                Person person = new Person(name,age);// създаваме обект
+                people.add(person);
+            }
+        }
+        people.sort(Comparator.comparing(p -> p.getName()));
+        for (Person person : people) {
+            System.out.println(person.toString());
+        }
+    }
+
+}
